9be308412d22ccf9c1428c4f6b1260e22743fcc8
GienekGenin <genin.gennadiigendos2010@yandex.ru>
2020-01-24 10:13:03 +0100

    pm2 setup

--- src/common/services/instance.kill.service.ts
+++ src/common/services/instance.kill.service.ts
@@ -2,14 +2,12 @@ export const listenToKill = () => {
   process.on('uncaughtException', e => {
     // tslint:disable-next-line:no-console
     console.log(e);
-    console.log(1111);
     process.exit(1);
   });
 
   process.on('unhandledRejection', e => {
     // tslint:disable-next-line:no-console
     console.log(e);
-    console.log(1111);
     process.exit(1);
   });
 };
