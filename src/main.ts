1cbdd9d1e9d0af932705d2548e6a555d9d6bd894
GienekGenin <genin.gennadiigendos2010@yandex.ru>
2020-01-24 15:03:02 +0100

    applyMiddleware + redis

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,25 @@
-import { AllExceptionsFilter } from './common/middleware/all-exceptions.filter';
-import { TransformInterceptor } from './common/middleware/transform.interceptor';
 import { listenToKill } from './common/services/instance.kill.service';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
-import * as helmet from 'helmet';
-import * as logger from 'morgan';
 import { configService } from './config/config.service';
-import * as cors from 'cors';
 import { createTypeOrmCOnfig } from './config/typeorm.config-export.service';
+import { Transport } from '@nestjs/microservices';
+import { applyMiddleware } from './common/services/app.config-service';
+
 listenToKill();
 createTypeOrmCOnfig();
 const bootstrap = async () => {
   try {
     const app = await NestFactory.create(AppModule);
 
-    app.use(
-      cors(),
-      helmet(),
-      logger('dev'),
-      bodyParser.json({ limit: 50000000, type: 'application/json' }),
-      bodyParser.urlencoded({ extended: true }),
-      bodyParser.text(),
-    );
+    app.connectMicroservice({
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379',
+      },
+    });
 
-    app.useGlobalFilters(new AllExceptionsFilter());
-    app.useGlobalInterceptors(new TransformInterceptor());
-    const options = new DocumentBuilder()
-      .setTitle('App API docs')
-      .setDescription('API description')
-      .setVersion('1.0')
-      .addTag('tag')
-      .build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api', app, document);
+    applyMiddleware(app);
 
     const port = configService.getPort();
 
